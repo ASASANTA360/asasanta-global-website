a723f8dd4d3c4274f3a8d3c02f3703f03d90562e
Salisu Ishaq Asasanta <asasantaglobaltech@gmail.com>
2026-05-26 15:22:39 -0700

    initial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,5 @@
+export async function POST() {
+  return Response.json({
+    message: "AI chat temporarily disabled",
+  });
+}
---
+++ lib/mongodb.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function connectDB() {
+  if (!MONGODB_URI) {
+    console.log("MongoDB URI not found, skipping DB connection.");
+    return null;
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+export default connectDB;
